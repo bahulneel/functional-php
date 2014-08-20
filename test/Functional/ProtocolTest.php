a5b7caf2d7c5cae4261e86ab4fb6c29d84a23466
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2014-08-20 12:47:00 +0100

    Added class constant use for 5.5+

--- test/Functional/ProtocolTest.php
+++ test/Functional/ProtocolTest.php
@@ -7,13 +7,20 @@ use PHPUnit_Framework_TestCase;
 
 class ProtocolTest extends PHPUnit_Framework_TestCase
 {
-    public function setUp()
+
+    public static function setUpBeforeClass()
     {
-        Sequence::extend("array", "BahulNeel\Functional\ArraySequence");
-        Sequence::extend("NULL", "BahulNeel\Functional\NullSequence");
-        Sequence::extend("ArrayObject", "BahulNeel\Functional\ArrayObjectSequence");
+        if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+            Sequence::extend("array", "BahulNeel\Functional\ArraySequence");
+            Sequence::extend("NULL", "BahulNeel\Functional\NullSequence");
+            Sequence::extend("ArrayObject", "BahulNeel\Functional\ArrayObjectSequence");
+        } else {
+            Sequence::extend("array", ArraySequence::class);
+            Sequence::extend("NULL", NullSequence::class);
+            Sequence::extend("ArrayObject", ArrayObjectSequence::class);
+        }
     }
-    
+
     /**
      * @expectedException \RuntimeException
      */
@@ -21,7 +28,7 @@ class ProtocolTest extends PHPUnit_Framework_TestCase
     {
         BadProtocol::extend("ArrayObject", "BahulNeel\Functional\BadSequence");
     }
-    
+
     /**
      * @expectedException InvalidArgumentException
      */
@@ -29,52 +36,57 @@ class ProtocolTest extends PHPUnit_Framework_TestCase
     {
         Sequence::extend("ArrayObject", "BahulNeel\Functional\BadSequence");
     }
-    
+
     public function testArray()
     {
         $array = [1, 2, 3, 4];
         $this->assertEquals(1, Sequence::first($array));
         $this->assertEquals([2, 3, 4], Sequence::rest($array));
     }
-    
+
     public function testNull()
     {
         $this->assertEquals("nil", Sequence::first(null));
         $this->assertEquals("also nil", Sequence::rest(null));
     }
-    
+
     public function testArrayObject()
     {
         $array = new ArrayObject([1, 2, 3, 4]);
         $this->assertEquals(1, Sequence::first($array));
         $this->assertEquals([2, 3, 4], Sequence::rest($array)->getArrayCopy());
     }
+
 }
 
 interface SequenceInterface
 {
+
     public static function first($coll);
-    
+
     public static function rest($coll);
 }
 
-class Sequence implements SequenceInterface 
+class Sequence implements SequenceInterface
 {
+
     use Protocol;
 
     public static function first($coll)
     {
         return self::invoke(__FUNCTION__, func_get_args());
     }
-    
+
     public static function rest($coll)
     {
         return self::invoke(__FUNCTION__, func_get_args());
     }
+
 }
 
 class ArraySequence implements SequenceInterface
 {
+
     public static function first($coll)
     {
         if (!count($coll)) {
@@ -93,14 +105,17 @@ class ArraySequence implements SequenceInterface
 
 class ArrayObjectSequence extends ArraySequence
 {
+
     public static function rest($coll)
     {
         return new ArrayObject(Sequence::rest($coll->getArrayCopy()));
     }
+
 }
 
 class NullSequence implements SequenceInterface
 {
+
     public static function first($coll)
     {
         return "nil";
@@ -115,6 +130,7 @@ class NullSequence implements SequenceInterface
 
 class BadProtocol
 {
+
     use Protocol;
 }
 
@@ -122,4 +138,3 @@ class BadSequence
 {
     
 }
-
